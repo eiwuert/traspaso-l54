1645a03cefed8d315360534049c7c20b57b1ffb3
Katherine Inalef <katherineinalef@rfc1918-ignorant.gov.cl>
2017-09-20 16:20:49 -0300

    Label que indica si la sección cuenta o no con información ingresada

--- resources/views/backend/actas/iniciar.blade.php
+++ resources/views/backend/actas/iniciar.blade.php
@@ -4,10 +4,20 @@
 
 <div class="row">
 
+
 	<div class="col-md-12">
 
 		<br>
 		@include('layouts.vista')
+		<div class="col-md-12">
+			<br>
+			@if (Session::has('message'))
+				<div class="alert alert-info">{{ Session::get('message') }}</div>
+			@endif
+			@if (Session::has('error')  )
+				<div class="alert alert-danger">{{ Session::get('error') }}</div>
+			@endif
+		</div>
 		<div class="page-header">
 			<h2>Acta Administrativa de Entrega</h2>
 		</div>
@@ -17,10 +27,16 @@
 			<li class="list-group-item">
 				<div class="row">
 					<div class="col-md-10">
-						<h4>1. Entrega Programática, gestión</h4>
+						<h4>1. Entrega Programática, gestión
+							<small>
+								@if($gestion) <span class="label label-info">Con información</span> 
+								@else <span class="label label-warning">Sin información</span>
+								@endif
+							</small>
+						</h4>
 					</div>
 					<div class="col-md-2">
-						<a href="{{ (isset($acta_id))? url('/backend/actas/gestion/'.$acta_id) :  url('/backend/actas/gestion')}}" class="btn btn-warning btn-lg">Editar</a>
+						<a href="{{ (isset($acta_id))? url('/backend/actas/gestion/'.$acta_id) :  url('/backend/actas/gestion')}}" class="btn btn-primary btn-lg">Editar</a>
 					</div>
 				</div>
 				
@@ -28,10 +44,17 @@
 			<li class="list-group-item">
 				<div class="row">
 					<div class="col-md-10">
-						<h4>2. Entrega Área de Personal</h4>
+						<h4>2. Entrega Área de Personal 
+							<small>
+								@if($personal) <span class="label label-info">Con información</span> 
+								@else <span class="label label-warning">Sin información</span>
+								@endif
+							</small>
+						
+						</h4>
 					</div>
 					<div class="col-md-2">
-						<a href="{{ (isset($acta_id))? url('/backend/actas/personal/'.$acta_id) :  url('/backend/actas/personal')}}" class="btn btn-warning btn-lg">Editar</a>
+						<a href="{{ (isset($acta_id))? url('/backend/actas/personal/'.$acta_id) :  url('/backend/actas/personal')}}" class="btn btn-primary btn-lg">Editar</a>
 					</div>
 				</div>
 				
@@ -40,10 +63,17 @@
 			<li class="list-group-item">
 				<div class="row">
 					<div class="col-md-10">
-						<h4>3. Entrega Área Financiera</h4>
+						<h4>3. Entrega Área Financiera
+							<small>
+								@if($financiera) <span class="label label-info">Con información</span> 
+								@else <span class="label label-warning">Sin información</span>
+								@endif
+							</small>
+						
+						</h4>
 					</div>
 					<div class="col-md-2">
-						<a href="{{ (isset($acta_id))? url('/backend/actas/financiera/'.$acta_id) :  url('/backend/actas/financiera')}}" class="btn btn-warning btn-lg">Editar</a>
+						<a href="{{ (isset($acta_id))? url('/backend/actas/financiera/'.$acta_id) :  url('/backend/actas/financiera')}}" class="btn btn-primary btn-lg">Editar</a>
 					</div>
 				</div>
 				
@@ -51,10 +81,17 @@
 			<li class="list-group-item">
 				<div class="row">
 					<div class="col-md-10">
-						<h4>4. Entrega Área Inventario</h4>
+						<h4>4. Entrega Área Inventario
+							<small>
+								@if($inventario) <span class="label label-info">Con información</span> 
+								@else <span class="label label-warning">Sin información</span>
+								@endif
+							</small>
+						
+						</h4>
 					</div>
 					<div class="col-md-2">
-						<a href="{{ (isset($acta_id))? url('/backend/actas/inventario/'.$acta_id) :  url('/backend/actas/inventario')}}" class="btn btn-warning btn-lg">Editar</a>
+						<a href="{{ (isset($acta_id))? url('/backend/actas/inventario/'.$acta_id) :  url('/backend/actas/inventario')}}" class="btn btn-primary btn-lg">Editar</a>
 					</div>
 				</div>
 				
@@ -62,10 +99,17 @@
 			<li class="list-group-item">
 				<div class="row">
 					<div class="col-md-10">
-						<h4>5. Entrega Área Legal</h4>
+						<h4>5. Entrega Área Legal
+							<small>
+								@if($legal) <span class="label label-info">Con información</span> 
+								@else <span class="label label-warning">Sin información</span>
+								@endif
+							</small>
+						
+						</h4>
 					</div>
 					<div class="col-md-2">
-						<a href="{{ (isset($acta_id))? url('/backend/actas/legal/'.$acta_id) :  url('/backend/actas/legal')}}" class="btn btn-warning btn-lg">Editar</a>
+						<a href="{{ (isset($acta_id))? url('/backend/actas/legal/'.$acta_id) :  url('/backend/actas/legal')}}" class="btn btn-primary btn-lg">Editar</a>
 					</div>
 				</div>
 				
@@ -73,11 +117,17 @@
 			<li class="list-group-item">
 				<div class="row">
 					<div class="col-md-10">
-						<h4>6. Entrega Área Administrativa</h4>
+						<h4>6. Entrega Área Administrativa
+							<small>
+								@if($administrativa) <span class="label label-info">Con información</span> 
+								@else <span class="label label-warning">Sin información</span>
+								@endif
+							</small>
+						
+						</h4>
 					</div>
 					<div class="col-md-2">
-						<!--<a href="{{ (isset($acta_id))? url('/backend/actas/administrativa/'.$acta_id) :  url('/backend/actas/administrativa')}}" class="btn btn-warning btn-lg">Editar</a>-->
-						<a href="{{url('/backend/actas/administrativa')}}" class="btn btn-warning btn-lg">Editar</a>
+						<a href="{{ (isset($acta_id))? url('/backend/actas/administrativa/'.$acta_id) :  url('/backend/actas/administrativa')}}" class="btn btn-primary btn-lg">Editar</a>
 					</div>
 				</div>
 				
@@ -85,10 +135,17 @@
 			<li class="list-group-item">
 				<div class="row">
 					<div class="col-md-10">
-						<h4>7. Entrega Área Auditoría Interna</h4>
+						<h4>7. Entrega Área Auditoría Interna
+							<small>
+								@if($auditoria) <span class="label label-info">Con información</span> 
+								@else <span class="label label-warning">Sin información</span>
+								@endif
+							</small>
+						
+						</h4>
 					</div>
 					<div class="col-md-2">
-						<a href="{{ (isset($acta_id))? url('/backend/actas/auditoria/'.$acta_id) :  url('/backend/actas/auditoria')}}" class="btn btn-warning btn-lg">Editar</a>
+						<a href="{{ (isset($acta_id))? url('/backend/actas/auditoria/'.$acta_id) :  url('/backend/actas/auditoria')}}" class="btn btn-primary btn-lg">Editar</a>
 					</div>
 				</div>
 				
@@ -96,11 +153,17 @@
 			<li class="list-group-item">
 				<div class="row">
 					<div class="col-md-10">
-						<h4>8. Instancias de participación ciudadana</h4>
+						<h4>8. Instancias de participación ciudadana
+							<small>
+								@if($participacion) <span class="label label-info">Con información</span> 
+								@else <span class="label label-warning">Sin información</span>
+								@endif
+							</small>
+						
+						</h4>
 					</div>
 					<div class="col-md-2">
-						<!--<a href="{{ (isset($acta_id))? url('/backend/actas/participacion/'.$acta_id) :  url('/backend/actas/participacion')}}" class="btn btn-warning btn-lg">Editar</a>-->
-						<a href="{{url('/backend/actas/participacion')}}" class="btn btn-warning btn-lg">Editar</a>
+						<a href="{{ (isset($acta_id))? url('/backend/actas/participacion/'.$acta_id) :  url('/backend/actas/participacion')}}" class="btn btn-primary btn-lg">Editar</a>
 					</div>
 				</div>
 				
@@ -108,11 +171,31 @@
 			<li class="list-group-item">
 				<div class="row">
 					<div class="col-md-10">
-						<h4>9. Otros antecedentes</h4>
+						<h4>9. Biblioteca Digital del Gobierno de Chile
+
+						
+						</h4>
 					</div>
 					<div class="col-md-2">
-						<!--<a href="{{ (isset($acta_id))? url('/backend/actas/otro/'.$acta_id) :  url('/backend/otro/gestion')}}" class="btn btn-warning btn-lg">Editar</a>-->
-						<a href="{{url('/backend/actas/otro')}}" class="btn btn-warning btn-lg">Editar</a>
+						<a href="{{ (isset($acta_id))? url('/backend/actas/biblioteca/'.$acta_id) :  url('/backend/actas/biblioteca')}}" class="btn btn-primary btn-lg">Editar</a>
+					</div>
+				</div>
+				
+			</li>
+			<li class="list-group-item">
+				<div class="row">
+					<div class="col-md-10">
+						<h4>10. Otros antecedentes
+							<small>
+								@if($otros) <span class="label label-info">Con información</span> 
+								@else <span class="label label-warning">Sin información</span>
+								@endif
+							</small>
+						
+						</h4>
+					</div>
+					<div class="col-md-2">
+						<a href="{{ (isset($acta_id))? url('/backend/actas/otro/'.$acta_id) :  url('/backend/otro/gestion')}}" class="btn btn-primary btn-lg">Editar</a>
 					</div>
 				</div>
 				
