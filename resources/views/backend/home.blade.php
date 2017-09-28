979b8ec572f7be801209733f0e07365978ac37eb
Katherine Inalef <katherineinalef@rfc1918-ignorant.gov.cl>
2017-09-28 16:31:26 -0300

    Modificación texto

--- resources/views/backend/home.blade.php
+++ resources/views/backend/home.blade.php
@@ -7,17 +7,17 @@
 
 		<div class="col-md-12">
 			<h2>Traspaso de Gobierno</h2>
-			<p class="lead">Bienvenido a la plataforma que le permitirá generar el acta administrativa de entrega.</p>
+			<p class="lead">Bienvenido a la plataforma que le permitirá generar su acta administrativa de entrega.</p>
 			<br>
 			
 		</div>
 		<br>
 		<div class="col-md-6 col-md-offset-3">
 			<div class="jumbotron">
-				<h3>Actas de entrega</h3>
-	            <p>Formulario Electrónico de generación de actas de entrega.</p>
+				<h3>Acta de entrega</h3>
+	            <p>Formulario Electrónico de generación de acta de entrega.</p>
 
-	            <a class="btn btn-lg btn-info btn-block" href="<?php echo url('backend/actas/listado');?>">Ver Actas de entrega</a>
+	            <a class="btn btn-lg btn-info btn-block" href="<?php echo url('backend/actas/listado');?>">Ver Acta de entrega</a>
 			</div>
 
 		</div>
