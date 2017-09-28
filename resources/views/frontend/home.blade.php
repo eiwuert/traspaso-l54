e71c970031feb7963b82ee9b52026c6c65e30cf2
Katherine Inalef <katherineinalef@rfc1918-ignorant.gov.cl>
2017-09-28 16:39:54 -0300

    Modificación texto

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -16,7 +16,7 @@
 
 	<div class="col-lg-8">
 	    <h1>Plataforma Traspaso<br> de Gobierno</h1>
-	    <p class="lead" style="font-size: 21px">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+	    <p class="lead" style="font-size: 21px">Plataforma disponible a contar de octubre de 2017.</p>
 	  
 		
 		<p class="text-center lead">
